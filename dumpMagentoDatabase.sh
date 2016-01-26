b2891c9a2b2c3220d64aeb3b4d18f20ec58637b4
Paul Millband <millband_paul@hotmail.co.uk>
2016-01-26 08:09:32 +0000

    shell basic outline of idea ... now to test

--- dumpMagentoDatabase.sh
+++ dumpMagentoDatabase.sh
@@ -1,12 +1,19 @@
-truncateRewrites=true
-databaseRef='test'
+
+databaseRef='test'     ###### what should this be
+
+
+if [ -z "${truncateRewrites}" ]; then
+	truncateRewrites=true  ######### should this set to yes by default
+fi
 
 if [ -z "${magentoPath}" ]; then
-	magentoPath=$1
+	magentoPath='~/public_html'
+fi
+if [ -z "${folderPath}" ]; then
+	folderPath='/tmp/databases'
 fi
 
 date=`date +%Y-%m-%d`;
-folderPath='/tmp/database'
 filName="{databaseRef}-${date}.sql"
 filePath="${folderPath}/${fileName}"
 
@@ -20,18 +27,22 @@ if [ ! -d "${folderPath}" ] ; then
 	mkdir --parents ${folderPath}
 fi
 
-if [ ! -w "$dnam" ] ; then
+if [ ! -w "${folderPath}" ] ; then
 	echo "${folderPath} is not writable"
 	exit
 fi
 
 if [ !(-a "${filePath}") ] ; then
-	touch "${filePath}.lock" &&
-	rm "${folderPath}/${databaseRef}-*.tar.gz"
-	cd ${magentoPath}
-	/tmp/n98-magerun.phar db:dump --strip="$truncateTablesList" ${filePath} &&
-	tar -czf "${filePath}.tar.gz" --directory ${folderPath} $file &&
-	rm -f "${filePath}.lock" &&
-	rm ${filePath}
+	if [ !(-e "${filePath}.lock") ] ; then
+		touch "${filePath}.lock" &&
+		rm "${folderPath}/${databaseRef}-*.tar.gz"
+		cd ${magentoPath}
+		/tmp/n98-magerun.phar db:dump --strip="$truncateTablesList" ${filePath} &&
+		tar -czf "${filePath}.tar.gz" --directory ${folderPath} $file &&
+		rm -f "${filePath}.lock" &&
+		rm ${filePath}
+	fi
 fi
 
+unset magentoPath
+unset folderPath
--- getAllDatabase.sh
+++ getAllDatabase.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+siteLoginList=( "test@example.com" "test@example2.com" )
+example_com='/var/www/htdocs'
+example2_com='/var/www/public_html'
+
+for siteLogin in "${siteLoginList[@]}" ; do
+	safeDomain=${siteLogin##*@} #remove all text before the line
+	safeDomain=$( echo ${safeDomain} | sed 's/[^a-zA-Z0-9_]/_/g'
+	magentoPath="${!safeDomain}"
+	dumpScript=`cat dumpMagentoDatabase.sh`
+	# or do this
+	#dumpScript=$(<dumpMagentoDatabase.sh)
+
+	ssh ${siteLogin} "magentoPath=${magentoPath} ; ${dumpScript}"
+
+	outputFolder="./databases/${safeDomain}"
+	if [ ! -d "${outputFolder}" ] ; then
+		mkdir --parents ${outputFolder}
+	fi
+	if [ ! -w "${outputFolder}" ] ; then
+		echo "${outputFolder} is not writable"
+		exit
+	fi
+
+	rsync -ahz ${siteLogin}:/tmp/databases/* ${outputFolder}
+done
+
+
